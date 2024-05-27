aa4141dbcfec7f84253d8574f3f8052e636797db
Polo_a <kiviku7@mail.ru>
2024-05-27 11:04:24 +0300

    programm 14

---
+++ src/Exceptions/Exceptions/src/Exceptions/UncorrectDataException.java
@@ -0,0 +1,8 @@
+package Exceptions.Exceptions.src.Exceptions;
+
+public class UncorrectDataException extends RuntimeException{
+    
+    public UncorrectDataException (String msg) {
+        super(msg);
+    }
+}
